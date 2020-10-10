e687a63cea2b63179046f61bc7215a06442c1a2a
KMDEV22 <70811830+KMDEV22@users.noreply.github.com>
2020-10-10 21:14:45 +0530

    Adding style and script as external files(link,scriptsource)

---
+++ clock.js
@@ -0,0 +1,6 @@
+setInterval(showTime, 1000)
+function showTime(){
+    let date= new Date()
+    document.querySelector(".digits").textContent=date.toLocaleTimeString()
+    console.log(date.toLocaleTimeString())
+}
